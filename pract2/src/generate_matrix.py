14128d6cf5b43f409bc68e9841ac162a0b52fcc7
Octocat& <3537256lol@gmail.com>
2026-03-05 23:17:53 +0700

    also generate right part of a vector

--- pract2/src/generate_matrix.py
+++ pract2/src/generate_matrix.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from random import random
+from math import pi, sin
 
-
-def generate_matrix(N):
+# Au = b
+def symmetric(N):
     a = [0] * N*N
     for i in range(N):
         for j in range(N):
@@ -11,17 +12,33 @@ def generate_matrix(N):
             elif (j < i): # под главной диагональю
                 a[i*N + j] = a[j*N + i] 
             else:  # на диагонали
-                a[i*N + j] = N
-    return a
+                a[i*N + j] = N  
+    
+    u = [sin(2 * pi * i / N) for i in range(1, N+1)]
+    b = [a[i] * u[i] for i in range(N) ]
+    return a, b
+
+# Ax = b, x should be (1,1,1,1,1,1,...)
+def test(N):
+    a = [2 if i == j else 1 for i in range(N) for j in range(N)]
+    b = [N+1] * N
+    return a,b
 
 N = 10
-matrix = generate_matrix(N)
+#matrix, b = symmetric(N)
+matrix, b = test(N)
 
-# сохранить сгенерированную матрицу
+# сохранить сгенерированную матрицу и вектор b (правая часть)
 with open('matrix.txt', 'w') as f:
     f.write(f"{N}\n")
     for i in range(N):
         for j in range(N):
             x = matrix[i*N + j]
-            f.write(f"{x:.2f} ") 
+            f.write(f"{x} ") 
         f.write('\n')
+
+    for num in b:
+        f.write(f"{num} ")
+
+
+    
