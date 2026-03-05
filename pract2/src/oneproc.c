e6330dae84bd76d5908b94cdf73385d2023fe9e9
Octocat& <3537256lol@gmail.com>
2026-03-05 23:19:48 +0700

    read vector and matrix

--- pract2/src/oneproc.c
+++ pract2/src/oneproc.c
@@ -10,10 +10,19 @@ int main() {
     fscanf(f, "%lu", &N);
     
     double* m = (double*) malloc(sizeof(double) * N * N);
+    double* b = (double*) malloc(sizeof(double) * N);
+    if (!m || !b) {
+        perror("Cannot allocate data");
+        if (m) free(m);
+        if (b) free(b);
+        exit(1);
+    }
+
     read_matrix(f, m, N);
-
-
-
+    read_vector(f, b, N);
+    
+    log_matrix("a", m, N, true);
+    log_vector("b", b, N, true);
     // free resources
     fclose(f);
     free(m);
