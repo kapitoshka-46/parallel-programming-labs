f1bf2b44c4acf2f8d510615ba6a4df708668df44
Octocat& <3537256lol@gmail.com>
2026-03-06 02:12:27 +0700

    improve logging

--- pract2/src/utility.h
+++ pract2/src/utility.h
@@ -3,6 +3,7 @@
 #include <stddef.h>  // for size_t
 #include <stdio.h>
 #include <stdlib.h> // for FILE
+#include <string.h>
 
 // Reads N*N doubles form file
 void read_matrix(FILE* f, double* buf, size_t N) {
@@ -27,7 +28,7 @@ void log_matrix(const char* name, double* m, size_t N, bool to_stderr) {
     mode = cnt == 0 ? "w" : "a";
 
     FILE* f = to_stderr ? stderr : fopen("log.txt", "w");
-    fprintf(f, "%d. %s:\n", cnt, name);
+    fprintf(f, "\n[matrix %d] %s:\n", cnt, name);
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++) {
             fprintf(f, "%.2lf ", m[i * N + j]);
@@ -46,7 +47,7 @@ void log_vector(const char* name, double* m, size_t N, bool to_stderr) {
     mode = cnt == 0 ? "w" : "a";    // first create file and then append
 
     FILE* f = to_stderr ? stderr : fopen("log_vector.txt", "w");
-    fprintf(f, "%d. %s:\n", cnt, name);
+    fprintf(f, "\n[vector %d] %s:\n", cnt, name);
     for(int i = 0; i < N; i++) {
         fprintf(f, "%.2lf ", m[i]);
     }
