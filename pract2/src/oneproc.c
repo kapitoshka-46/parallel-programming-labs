16eb3c58b71d99adc22ca60c6f38f27e732ea27c
Octocat& <3537256lol@gmail.com>
2026-03-05 22:34:54 +0700

    move log_matrix to utility

--- pract2/src/oneproc.c
+++ pract2/src/oneproc.c
@@ -2,18 +2,6 @@
 #include "utility.h"
 #include <stdbool.h>
 
-void log_matrix(const char* name, double* m, size_t N, bool to_stderr) {
-    
-    FILE* f = to_stderr ? stderr : fopen("log.txt", "w");
-    fprintf(f, "%s:\n", name);
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            fprintf(f, "%.2lf ", m[i * N + j]);
-        }
-        fprintf(f, "\n");
-    }
-    if (!to_stderr) fclose(f); 
-}
 
 int main() {
     size_t N;
@@ -24,8 +12,6 @@ int main() {
     double* m = (double*) malloc(sizeof(double) * N * N);
     read_matrix(f, m, N);
 
-    
-    
 
 
     // free resources
--- pract2/src/utility.h
+++ pract2/src/utility.h
@@ -11,3 +11,16 @@ void read_matrix(FILE* f, double* buf, size_t N) {
         int r = fscanf(f, "%lf", &buf[i]);
     }    
 }
+
+void log_matrix(const char* name, double* m, size_t N, bool to_stderr) {
+    
+    FILE* f = to_stderr ? stderr : fopen("log.txt", "w");
+    fprintf(f, "%s:\n", name);
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            fprintf(f, "%.2lf ", m[i * N + j]);
+        }
+        fprintf(f, "\n");
+    }
+    if (!to_stderr) fclose(f); 
+}
